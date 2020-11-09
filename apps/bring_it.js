51780ac87ed99430b1814e62b97d546e3aa2e8b3
IvoMart <ivo_ivancho@hotmail.com>
2020-11-09 14:59:02 -0300

    HTML Javascript Basico (-.-)

---
+++ apps/bring_it.js
@@ -0,0 +1,8 @@
+/*var numeroUno = 5;
+var numeroDos = 20;*/
+
+var resultado = 0;
+for (var i = 0; i < 3; i++) {
+    resultado = i + (i + 2);
+    console.log(`El resultado de la suma es:.. ${resultado}`);
+};
